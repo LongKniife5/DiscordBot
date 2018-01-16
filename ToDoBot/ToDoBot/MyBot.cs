eea89f8c7ec01fbd84e666f4e073b9988a1f4d80
LongKnife5 <31137673+LongKniife5@users.noreply.github.com>
2018-01-16 08:39:00 -0700

    copied from botmyst

--- ToDoBot/ToDoBot/MyBot.cs
+++ ToDoBot/ToDoBot/MyBot.cs
@@ -1,262 +1,82 @@
 ﻿using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 
 using System;
 using System.IO;
-using System.Text;
+using System.Linq;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Collections;
 using System.Runtime.Serialization.Formatters.Binary;
 
-
 namespace ToDoBot
 {
-	class MyBot
+	public class MyBot
 	{
-		DiscordClient discord;
+		private DiscordSocketClient client;
+		private CommandService commands;
+		private IServiceProvider services;
+
+		public static readonly string CommandPrefix = "."; 
 
 		List<string> toDoList = new List<string>();
 
-		public MyBot()
+		public async Task Start()
 		{
-			#region IHonestlyCouldn'tTellYouWhatThisDoes
-			discord = new DiscordClient(x =>
-			{
-				x.LogLevel = LogSeverity.Info;
-				x.LogHandler = Log;
-			});
-			#endregion
+			client = new DiscordSocketClient();
+			commands = new CommandService();
 
-			#region ThisSetsUpTheCommandPrefix
-			discord.UsingCommands(x =>
-			{
-				x.PrefixChar = '.';
-				x.AllowMentionPrefix = true;
-			});
-			#endregion
+			services = new ServiceCollection()
+				.AddSingleton(commands)
+				.BuildServiceProvider();
+
+			await InstallCommands();
 			
-			//i'm not sure, but it is probably important
-			var commands = discord.GetService<CommandService>();
-			
-			#region ConversationCommands
-			commands.CreateCommand("hello")
-				.Do(async (e) => {
-					await e.Channel.SendMessage("Leave me alone " + e.User.Name + " I know where you live!!");
-			});
+			await client.LoginAsync(TokenType.Bot, put token here);
+			await client.SetGameAsync("hi");//i think it will say this
+			await client.StartAsync();
 
-			commands.CreateCommand("How Ya Doin?")
-				.Do(async (e) => {
-					await e.Channel.SendMessage("Shut up and get back to work " + e.User.Name + "!");
-				});
+			Console.WriteLine("ready");
 
-			commands.CreateCommand("Idea")
-				.Do(async (e) => {
-					await e.Channel.SendMessage(e.User.Name + " that is your job to find!");
-				});
-			#endregion
+			await Task.Delay(-1);
+		}
 
-			#region listCommand
-			commands.CreateCommand("list")
-				.Do(async (e) => {
-					PurgeChannel(e);
-				});
-			#endregion
+		private async Task InstallCommands()
+		{
+			client.MessageReceived += HandleCommand;
+			await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+		}
 
-			#region todoCommand
-			commands.CreateCommand("todo").Parameter("listVar", ParameterType.Multiple)
-				.Do(async (e) => {
-					await AddToList(e);
-					PurgeChannel(e);
-				});
-			#endregion
+		private async Task HandleCommand(SocketMessage msg)
+		{
+			SocketUserMessage message = msg as SocketUserMessage;
+			if(message == null)
+				return;
+			int argPos = 0;
 
-			#region testCommand
-			commands.CreateCommand("test").Parameter("testSTring", ParameterType.Multiple)
-				.Do(async (e) => {
-					string message = "";
-					for (int i = 0; i < e.Args.Length; i++)
-					{
-						message += e.Args[i].ToString() + " ";
-					}
-					await e.Channel.SendTTSMessage(message);
-				});
-			#endregion
+			 if ((message.HasStringPrefix (CommandPrefix, ref argPos) || message.HasMentionPrefix (client.CurrentUser, ref argPos)) == false) return;
 
-			#region doneCommand
-			commands.CreateCommand("done").Parameter("toRemove", ParameterType.Multiple)
-				.Do(async (e) => {
-					await takeFromList(e);
-					PurgeChannel(e);
-				});
-			#endregion
+            CommandContext context = new CommandContext (client, message);
+            CommandInfo executedCommand = commands.Search (context, argPos).Commands [0].Command;
 
-			#region SaveCommand
-			commands.CreateCommand("Save")
-				.Do(async (e) => {
-					await e.Channel.SendMessage("Saving...");
-					Save(e);
-				});
-			#endregion
-
-			#region loadcommand
-			commands.CreateCommand("Load")
-				.Do(async (e) => {
-					await e.Channel.SendMessage("Loading...");
-					Load(e);
-				});
-			#endregion
-
-			#region deletecommand
-			commands.CreateCommand("Delete")
-				.Do(async (e) => {
-					await e.Channel.SendMessage("Deleting...");
-					Delete(e);
-				});
-			#endregion
-
-			commands.CreateCommand("doneAll")
-				.Do(async (e) => {
-
-					toDoList.Clear();
-
-				});
-
-            #region ThisLogsTheBotIn
-            discord.ExecuteAndWait(async () =>
+			IResult result = await commands.ExecuteAsync (context, argPos, services);
+            if (result.IsSuccess == false)
             {
-                await discord.Connect("NDAyMTg0MDUyMTUwMzcwMzE1.DT1IVg.X_oXo6Z42LBP_S6OHFuO1rsRuzg", TokenType.Bot);
-            });
-			#endregion
+                if (result.Error == CommandError.UnknownCommand)
+					return;
+                if (result.Error == CommandError.BadArgCount)
+                {
+                    EmbedBuilder eb = new EmbedBuilder ();
+                    eb.WithTitle ("ERROR: Bad argument count");
+                    eb.WithColor (Color.Red);
+                    await context.Channel.SendMessageAsync (string.Empty, false, eb);
+                    await commands.ExecuteAsync (context, $"command {executedCommand.Name}");
+                }
+                else
+                    await context.Channel.SendMessageAsync (result.ErrorReason);
+            }
 		}
-        
-		//Tells the console the state of the bot, not seen in discord
-		private void Log(object sender, LogMessageEventArgs e)
-		{
-			Console.WriteLine(e.Message);
-		}
-
-		//When the todo command is used, this is called, and this calls the constructmessage function 
-		//and adds what is returned into the list of thingsToDo
-		private async Task AddToList(CommandEventArgs e)
-		{
-			var message = ConstructMessage(e);
-			toDoList.Add(message);
-		}
-
-		private async Task Save(CommandEventArgs e)
-		{
-
-			string thisFile = Directory.GetCurrentDirectory();
-			string saveToPath = thisFile + "\\saveFile.txt";
-			//await e.Channel.SendMessage(saveToPath);
-
-			BinaryFormatter bf = new BinaryFormatter();
-			FileStream file = File.Create(saveToPath);
-
-			UserData data = new UserData();
-			int i = 0;
-			foreach (string item in toDoList)
-			{
-				data.TODOLIST.Add(toDoList[i]);
-				i++;
-			}
-
-			bf.Serialize(file, data);
-			file.Close();
-		}
-
-		private async Task Load (CommandEventArgs e)
-		{
-			if(File.Exists(Directory.GetCurrentDirectory() + "\\saveFile.txt"))
-			{
-				BinaryFormatter bf = new BinaryFormatter();
-				FileStream file = File.Open(Directory.GetCurrentDirectory() + "\\saveFile.txt", FileMode.Open);
-
-				UserData data = (UserData)bf.Deserialize(file);
-				file.Close();
-
-				int i = 0;
-				foreach (string item in data.TODOLIST)
-				{
-					toDoList.Add(data.TODOLIST[i]);
-					i++;
-				}
-			}
-		}
-
-		private async Task Delete(CommandEventArgs e)
-		{
-			if (File.Exists(Directory.GetCurrentDirectory() + "\\saveFile.txt"))
-			{
-				File.Delete(Directory.GetCurrentDirectory() + "\\saveFile.txt");
-			}
-		}
-
-		//When the command is used, this is called and this calls the constructmessage function
-		//and uses what is returned to remove that from the toDoList
-		private async Task takeFromList(CommandEventArgs e)
-		{
-			var message = ConstructMessage(e);
-
-			int n;
-			bool isNumeric = int.TryParse(message, out n);
-			if (isNumeric)
-			{
-				toDoList.Remove(toDoList[n-1]);
-			}
-			else
-			{
-				toDoList.Remove(message);
-			}
-			//await e.Channel.SendMessage(message + " is removed from the list");
-		}
-
-		//This turns what was said immediately after the 
-		//command into a string and returns the value
-		private string ConstructMessage(CommandEventArgs e)
-		{
-			string message = "";
-
-			//if what was said after the commands had spaces (ex. .test hello world) 
-			//this will make sure that all words are read, instead of just the first.
-			for (int i = 0; i < e.Args.Length; i++)
-			{
-				message += e.Args[i].ToString() + " ";
-			}
-			return message;
-		}
-
-		//Despite the name, this does two things...
-		//First it does clear a lot of what is in the channel, 100 messages to be precise as that is the limit
-		//However after that it displays the ToDoList, so that we can always see what is on the list
-		private async void PurgeChannel(CommandEventArgs e)
-		{
-			//clears the messages
-			Message[] messagesToDelete;
-			messagesToDelete = await e.Channel.DownloadMessages(100);
-
-			await e.Channel.DeleteMessages(messagesToDelete);
-
-
-			//displays the list
-			string message = "```List: \n=========================== \n";
-			int i = 0;
-			foreach (string item in toDoList)
-			{
-                message += "- ";
-				message += toDoList[i] + "\n";
-				i++;
-			}
-			message += "```";
-			await e.Channel.SendMessage(message);
-		}
-
 	}
-}
-
-[Serializable]
-class UserData
-{
-	public List<string> TODOLIST = new List<string>();
 }
--- ToDoBot/ToDoBot/Program.cs
+++ ToDoBot/ToDoBot/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace ToDoBot
 {
@@ -11,6 +7,9 @@ namespace ToDoBot
 		static void Main(string[] args)
 		{
 			MyBot bot = new MyBot();
+			await bot.Start();
+
+			await Task.Delay(-1);
 		}
 	}
 }
