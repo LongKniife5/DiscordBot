a7819e4112c2b2cb9a02f87237be82c85e71c48f
LongKnife5 <31137673+LongKniife5@users.noreply.github.com>
2018-01-16 13:02:19 -0700

    Revert "now try"
    This reverts commit 293117e0527a22fa10eafb4dcd1f5af695d0787d.

--- ToDoBot/ToDoBot/MyBot.cs
+++ ToDoBot/ToDoBot/MyBot.cs
@@ -16,6 +16,7 @@ namespace ToDoBot
 	{
 		private DiscordClient client;
 		private CommandService commands;
+		private IServiceProvider services;
 
 		public static readonly string CommandPrefix = "."; 
 
@@ -26,6 +27,10 @@ namespace ToDoBot
 			client = new DiscordClient();
 			commands = new CommandService();
 
+			services = new ServiceCollection()
+				.AddSingleton(commands)
+				.BuildServiceProvider();
+
 			await InstallCommands();
 			
 			await client.LoginAsync(TokenType.Bot, put token here);
@@ -35,6 +40,42 @@ namespace ToDoBot
 			Console.WriteLine("ready");
 
 			await Task.Delay(-1);
+		}
+
+		private async Task InstallCommands()
+		{
+			client.MessageReceived += HandleCommand;
+			await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+		}
+
+		private async Task HandleCommand(SocketMessage msg)
+		{
+			SocketUserMessage message = msg as SocketUserMessage;
+			if(message == null)
+				return;
+			int argPos = 0;
+
+			 if ((message.HasStringPrefix (CommandPrefix, ref argPos) || message.HasMentionPrefix (client.CurrentUser, ref argPos)) == false) return;
+
+            CommandContext context = new CommandContext (client, message);
+            CommandInfo executedCommand = commands.Search (context, argPos).Commands [0].Command;
+
+			IResult result = await commands.ExecuteAsync (context, argPos, services);
+            if (result.IsSuccess == false)
+            {
+                if (result.Error == CommandError.UnknownCommand)
+					return;
+                if (result.Error == CommandError.BadArgCount)
+                {
+                    EmbedBuilder eb = new EmbedBuilder ();
+                    eb.WithTitle ("ERROR: Bad argument count");
+                    eb.WithColor (Color.Red);
+                    await context.Channel.SendMessageAsync (string.Empty, false, eb);
+                    await commands.ExecuteAsync (context, $"command {executedCommand.Name}");
+                }
+                else
+                    await context.Channel.SendMessageAsync (result.ErrorReason);
+            }
 		}
 	}
 }
