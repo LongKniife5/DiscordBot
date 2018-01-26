858d54d01728fef78691a19c52f0b5bf9bb95ba2
LongKnife5 <JD3565@k12.sd.us>
2018-01-25 20:42:27 -0700

    98% sure it is working now

--- ToDoBot/ToDoBot/Program.cs
+++ ToDoBot/ToDoBot/Program.cs
@@ -1,15 +1,76 @@
-﻿using System.Threading.Tasks;
-
-namespace BotMyst
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+namespace ToDoBot
 {
     public class Program
     {
-        public static async Task Main ()
-        {
-            BotMyst bot = new BotMyst ();
-            await bot.Start ();
+        static void Main() => new Program().RunBotAsync().GetAwaiter().GetResult();
 
-            await Task.Delay (-1);
-        } 
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string botPrefix = "NDAyMTg0MDUyMTUwMzcwMzE1.DT1IVg.X_oXo6Z42LBP_S6OHFuO1rsRuzg";
+
+            _client.Log += Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, botPrefix);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if (message is null || message.Author.IsBot)
+                return;
+
+            int argPos = 0;
+
+            //here is where you change the prefix
+            if (message.HasStringPrefix(".", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+            }
+        }
     }
 }
